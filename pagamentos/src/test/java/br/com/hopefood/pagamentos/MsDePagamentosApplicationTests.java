9e41318cc7a37aeff0392402cfbcaa0236e16298
Ramon Bento <104931916+RamonBent@users.noreply.github.com>
2025-01-22 20:07:27 -0300

    Add files via upload

---
+++ pagamentos/src/test/java/br/com/hopefood/pagamentos/MsDePagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hopefood.pagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDePagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
